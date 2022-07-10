54ac21a2d200c6d68480a8cf4777bdd4c2e111d8
abteilung6 <moeller.tilman@gmail.com>
2022-07-10 12:07:44 +0200

    feat: nested queries

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -2,10 +2,14 @@ const graphql = require("graphql");
 const axios = require("axios");
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
-const users = [
-  { id: "23", firstName: "Bill", age: 20 },
-  { id: "47", firstName: "Samatha", age: 21 },
-];
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+  },
+});
 
 const UserType = new GraphQLObjectType({
   name: "User",
@@ -13,6 +17,16 @@ const UserType = new GraphQLObjectType({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
+    companyId: { type: GraphQLString },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        console.log(parentValue, args);
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then((response) => response.data);
+      },
+    },
   },
 });
 
